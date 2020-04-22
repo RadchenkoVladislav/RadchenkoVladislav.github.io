44d260504054d4cf02e59f8f26011c0d15e80d66
RadchenkoVladislav <vladislav.radchenko@promo.ru>
2020-04-23 02:38:35 +0300

    s

--- send.php
+++ send.php
@@ -2,7 +2,8 @@
 
 if(isset($_POST['submit'])){
 $to = "radchenk.o.v@yadnex.ru";; // Здесь нужно написать e-mail, куда будут приходить письма
-$from = $_POST['name']; // this is the sender's Email address
+$from = $_POST['radchenk.o@yadnex.ru']; // this is the sender's Email address
+$name = $_POST['name'];
 $surname = $_POST['surname'];
 $q1 = $_POST['q1'];
 $q2 = $_POST['q2'];
@@ -17,12 +18,66 @@ $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['m
 
 $headers = "From:" . $from;
 $headers2 = "From:" . $to;
-
+    
+/* Переменная, которая будет отправлена на почту со значениями, вводимых в поля */
+$mail_to_myemail = "Здравствуйте! 
+Было отправлено сообщение с сайта! 
+Имя отправителя: $name
+Фамилия: $surname;  
+    
 mail($to,$subject,$message,$headers);
 // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender - Отключено!
 echo "Сообщение отправлено. Спасибо Вам " . $name . "!";
-echo "<br /><br /><a href='https://epicblog.net'>Вернуться на сайт.</a>";
+echo "<br /><br /><a href='https://radchenkovladislav.github.io/index.html'>Вернуться на сайт.</a>";
 
 }
 
 ?>
+
+<?php 
+/*
+Форма обратной связи может получать сообщения с любых почтовых ящиков.
+Исправлена проблема кодировки при получении писем почтовым клиентом Outlook.
+Вы скачали её с сайта Epic Blog https://epicblog.net Заходите на сайт снова!
+ВНИМАНИЕ! Лучше всего в переменную myemail прописать почту домена, который использует сайт. А не mail.ru, gmail и тд.
+*/
+if(isset($_POST['submit'])){
+/* Устанавливаем e-mail Кому и от Кого будут приходить письма */   
+$to = "info(собака)epicblog.net"; // Здесь нужно написать e-mail, куда будут приходить письма   
+$from = "no-reply(собака)epicblog.net"; // Здесь нужно написать e-mail, от кого будут приходить письма, например no-reply(собака)epicblog.net
+ 
+/* Указываем переменные, в которые будет записываться информация с формы */
+$first_name = $_POST['first_name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$subject = "Форма отправки сообщений с сайта Epic Blog";
+     
+/* Проверка правильного написания e-mail адреса */
+if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email))
+{
+show_error("<br /> Е-mail адрес не существует");
+}
+     
+/* Переменная, которая будет отправлена на почту со значениями, вводимых в поля */
+$mail_to_myemail = "Здравствуйте! 
+Было отправлено сообщение с сайта! 
+Имя отправителя: $first_name
+E-mail: $email
+Номер телефона: $phone
+Текст сообщения: $message
+Чтобы ответить на письмо, создайте новое сообщение, скопируйте электронный адрес и вставьте в поле Кому.";  
+     
+$headers = "From: $from \r\n";
+     
+/* Отправка сообщения, с помощью функции mail() */
+mail($to, $subject, $mail_to_myemail, $headers . 'Content-type: text/plain; charset=utf-8');
+echo "Сообщение отправлено. Спасибо Вам " . $first_name . ", мы скоро свяжемся с Вами.";
+echo "<br /><br /><a href='https://epicblog.net'>Вернуться на сайт.</a>";
+}
+?>
+<!--Переадресация на главную страницу сайта, через 3 секунды-->
+<script language="JavaScript" type="text/javascript">
+function changeurl(){eval(self.location="https://epicblog.net");}
+window.setTimeout("changeurl();",3000);
+</script>
