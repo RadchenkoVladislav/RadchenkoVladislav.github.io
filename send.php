4c08a6dc385ad5077395907424d6415b415f5098
RadchenkoVladislav <vladislav.radchenko@promo.ru>
2020-04-23 02:32:18 +0300

    s

--- send.php
+++ send.php
@@ -4,6 +4,12 @@ if(isset($_POST['submit'])){
 $to = "radchenk.o.v@yadnex.ru";; // Здесь нужно написать e-mail, куда будут приходить письма
 $from = $_POST['name']; // this is the sender's Email address
 $surname = $_POST['surname'];
+$q1 = $_POST['q1'];
+$q2 = $_POST['q2'];
+$q3 = $_POST['q3'];
+$q4 = $_POST['q4'];
+$q5 = $_POST['q5'];
+$q6 = $_POST['q6'];
 $subject = "Форма отправки сообщений с сайта";
 $subject2 = "Copy of your form submission";
 $message = $first_name . " оставил сообщение:" . "\n\n" . $_POST['message'];
