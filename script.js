0f2c1dc82679eb3f54d9191b22b3698f420ff104
RadchenkoVladislav <vladislav.radchenko@promo.ru>
2020-04-24 10:30:57 +0300

    asd

--- script.js
+++ script.js
@@ -1,20 +1,52 @@
-$(document).ready(function() {
-
-	//E-mail Ajax Send
-	$("main").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
-
-});
+$mysqli = new mysqli ("localhost", "root", "123456789", "bigbr");
+    $mysqli->query ("SET NAMES 'utf8'");
+    $nameUch = $_POST['nameUch'];
+    $addressUch = $_POST['addressUch'];
+    $addressUch = $_POST['cityUch'];
+    $areaUch = $_POST['areaUch'];
+    $namePed = $_POST['namePed'];
+    $familPed = $_POST['familPed'];
+    $email = $_POST['email'];
+    $number = $_POST['number'];
+    $nameComand = $_POST['nameComand'];
+    $nameUchenik = $_POST['nameUchenik'];
+    $familUchenik = $_POST['familUchenik'];
+    $ageUchenik = $_POST['ageUchenik'];
+    $sorevn = $_POST['sorevn'];
+    $pitanie = $_POST['pitanie'];
+    $persObr = $_POST['persObr'];    
+    $mysqli->query ("INSERT INTO `reg` (
+          `id`,
+          `nameUch`,
+          `addressUch`,
+          `cityUch`,
+          `areaUch`,
+          `namePed`,
+          `familPed`,
+          `email`,
+          `number`,
+          `nameComand`,
+          `nameUchenik`,
+          `familUchenik`,
+          `ageUchenik`,
+          `optionsRadios`,
+          `pitanie`,
+          `persObr`) VALUES (NULL,
+          '$nameUch',
+          '$addressUch',
+          '$areaUch',
+          '$namePed',
+          '$familPed',
+          '$email',
+          '$number',
+          '$nameComand',
+          '$nameUchenik',
+          '$ageUchenik',
+          '$sorevn',
+          '$pitanie',
+          '$persObr')");
+if ($mysqli == 'true'){
+          echo "Запись добавлена успешно!";
+       } else {
+          echo "Запись не добавлена!";
+      }
