252a4a7c2063404608a9178d028421955bc33e4f
RadchenkoVladislav <vladislav.radchenko@promo.ru>
2020-04-24 14:00:31 +0300

    asd

--- send.php
+++ send.php
@@ -1,54 +1,22 @@
 <?php
-$mysqli = new mysqli ("localhost", "root", "123456789", "bigbr");
-    $mysqli->query ("SET NAMES 'utf8'");
-    $nameUch = $_POST['name'];
-    $addressUch = $_POST['surname'];
-    $addressUch = $_POST['cityUch'];
-    $areaUch = $_POST['areaUch'];
-    $namePed = $_POST['namePed'];
-    $familPed = $_POST['familPed'];
-    $email = $_POST['email'];
-    $number = $_POST['number'];
-    $nameComand = $_POST['nameComand'];
-    $nameUchenik = $_POST['nameUchenik'];
-    $familUchenik = $_POST['familUchenik'];
-    $ageUchenik = $_POST['ageUchenik'];
-    $sorevn = $_POST['sorevn'];
-    $pitanie = $_POST['pitanie'];
-    $persObr = $_POST['persObr'];    
-    $mysqli->query ("INSERT INTO `reg` (
-          `id`,
-          `nameUch`,
-          `addressUch`,
-          `cityUch`,
-          `areaUch`,
-          `namePed`,
-          `familPed`,
-          `email`,
-          `number`,
-          `nameComand`,
-          `nameUchenik`,
-          `familUchenik`,
-          `ageUchenik`,
-          `optionsRadios`,
-          `pitanie`,
-          `persObr`) VALUES (NULL,
-          '$nameUch',
-          '$addressUch',
-          '$areaUch',
-          '$namePed',
-          '$familPed',
-          '$email',
-          '$number',
-          '$nameComand',
-          '$nameUchenik',
-          '$ageUchenik',
-          '$sorevn',
-          '$pitanie',
-          '$persObr')");
-if ($mysqli == 'true'){
-          echo "Запись добавлена успешно!";
-       } else {
-          echo "Запись не добавлена!";
-      }
+
+if(isset($_POST['submit'])){
+$to = "radchenk.o.v@yadnex.ru";; // Здесь нужно написать e-mail, куда будут приходить письма
+$from = $_POST['name']; // this is the sender's Email address
+$surname = $_POST['surname'];
+$subject = "Форма отправки сообщений с сайта";
+$subject2 = "Copy of your form submission";
+$message = $first_name . " оставил сообщение:" . "\n\n" . $_POST['message'];
+$message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+$headers = "From:" . $from;
+$headers2 = "From:" . $to;
+
+mail($to,$subject,$message,$headers);
+// mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender - Отключено!
+echo "Сообщение отправлено. Спасибо Вам " . $name . "!";
+echo "<br /><br /><a href='https://epicblog.net'>Вернуться на сайт.</a>";
+
+}
+
 ?>
